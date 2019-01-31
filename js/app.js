71864b2fb5858a7f0eb806a652283b04da57511d
Dylan Mooney <dylanmooney62@gmail.com>
2019-01-31 11:54:50 +0000

    added getFood and removeFood functionality

--- js/app.js
+++ js/app.js
@@ -1,30 +1,42 @@
 // EVENT LISTENERS
-document.getElementById('add-food-form').addEventListener('submit', function(e)  {
-  // stop form from submitting
+
+// ADD FOOD FORM FUNCTIONALITY
+document.getElementById('add-food-form').addEventListener('submit', function (e) {
   e.preventDefault();
 
-  // get all values from form
   const name = document.getElementById('add-food-name').value,
-        time = document.getElementById('add-food-time').value,
-        group = document.getElementById('add-food-group').value,
-        calories = parseInt(document.getElementById('add-food-calories').value),
-        description = document.getElementById('add-food-description').value;
+    time = document.getElementById('add-food-time').value,
+    group = document.getElementById('add-food-group').value,
+    calories = parseInt(document.getElementById('add-food-calories').value),
+    description = document.getElementById('add-food-description').value;
 
-  // create and add food from values
   addCurrentFood(new Food(name, calories, time, group, description));
 
-  // display food on home page
   displayCurrentFood();
 
-  // calculates total & remaing calories
   calculateTotalAndRemainingCalories();
 
-  // updates display
   updateCalorieDisplay();
 
-  // navigates to home page
   window.location.href = '#home';
+
+  this.reset();
 });
+
+
+// ADDING EVENT LISTENERS TO DYNAMICALLY CREATED FOOD
+document.getElementById('food-list').addEventListener('click', (e) => {
+  if(e.target.classList.contains('food')) {  
+    const food = getFoodByID(e.target.id);
+
+    if(food) {
+      displayFoodDetail(food);
+    }
+  }
+});
+
+
+
 
 
 
@@ -72,7 +84,7 @@ function getData() {
 }
 
 
-// ADDING AND DISPLAYING FOOD 
+// ADDING FOOD
 function addCurrentFood(food) {
   // get data object
   const data = getData();
@@ -84,45 +96,78 @@ function addCurrentFood(food) {
   localStorage.setItem('data', JSON.stringify(data));
 }
 
+// REMOVING FOOD
+function removeFood(id) {
+  const data = getData();
+  const foodList = data.food.currentFood;
+
+  const index = foodList.findIndex((e) => {
+   return e.id === id;
+  })
+
+  data.food.currentFood.splice(index, 1);
+
+  localStorage.setItem('data', JSON.stringify(data));
+}
+
+// DISPLAY CURRENT FOOD ON FOOD-LIST
 function displayCurrentFood() {
   const currentFood = getData().food.currentFood;
   const foodList = document.getElementById('food-list');
+  let html = '';
 
-  // reset foodList
-  foodList.innerHTML = '';
 
   // if there is food iterate through array creating template for each food item
   if (currentFood.length >= 1) {
-    for (food of currentFood) {
-      let html = `
-    <li class="food-list__item">
-      <a href="#food-detail" class="food" id="${food.id}">
-        <div class="food__container">
-          <div class="food__details food__details--left">
-           <span class="food__detail">${food.name}</span>
-          <span class="food__detail">${food.mealTime}</span>
-        </div>
-        <div class="food__details food__details--right">
-          <span class="food__detail">${food.calories}Kcal</span>
-          <span class="food__detail">${food.group}</span>
-         </div>
-        </div>
-      </a>
-    </li>`
-
-      foodList.insertAdjacentHTML('afterbegin', html);
+    // gets a copy of array and reverses it to display food items in most recent order
+    for (food of currentFood.slice().reverse()) {
+      html += `
+      <li class="food-list__item">
+            <a href="#food-detail" class="food" id="${food.id}">
+              <div class="food__container">
+                <div class="food__details">
+                  <h4 class="food__detail">${parseHtml(food.name)}</h4>
+                  <div class="food__detail">${food.calories}Kcal</div>
+                </div>
+                <div class="food__details">
+                  <div class="food__detail">${food.mealTime}</div>
+                  <div class="food__detail">${food.group}</div>
+                </div>
+              </div>
+            </a>
+          </li>`
     }
   } else {
-    foodList.innerHTML = `
+     html = `
     <li class="food-list-item">
     <p class="food-list__text">
-      Looks like you've not added any food for today. Press the plus button to add food.
+      Looks like you've not added any food for today.  touch the plus button to add food.
     </p>
     </li>
     `
   }
+  foodList.innerHTML = html;
 }
 
+// GET FOOD BY ID 
+function getFoodByID(id) {
+  const foodList = getData().food.currentFood;
+  return foodList.find((e) => {
+    return e.id === id;
+  })
+}
+
+
+
+// DISPLAYING DETAIL OF FOOD
+function displayFoodDetail(food) {
+  document.querySelector('.food--full-detail').id = food.id;
+  document.getElementById('food-detail-name').textContent = food.name;
+  document.getElementById('food-detail-calories').textContent = `${food.calories}Kcal`;
+  document.getElementById('food-detail-group').textContent = food.group;
+  document.getElementById('food-detail-time').textContent = food.mealTime;
+  document.getElementById('food-detail-description').textContent = food.description;
+}
 
 // SETTING CALORIE GOAL
 function setGoalCalories(goal) {
@@ -144,11 +189,11 @@ function calculateTotalCalories() {
 
   let totalCalories = 0;
 
-  
-    for (food of data.food.currentFood) {
-      totalCalories += food.calories;
-    }
-  
+
+  for (food of data.food.currentFood) {
+    totalCalories += food.calories;
+  }
+
   data.calories.total = totalCalories;
 
   localStorage.setItem('data', JSON.stringify(data));
@@ -195,6 +240,10 @@ function displayRemainingCalories() {
   carloriesRemainingDisplay.textContent = caloriesRemaining;
 }
 
+// HELPER FUNCTIONS
+function parseHtml(html) {
+ return html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
 
 window.onload = () => {
   displayCurrentFood();
